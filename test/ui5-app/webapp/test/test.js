30570645d09e44f70e57fc2d9d94df6cb4dd4e34
dominik.feininger <dominik.feininger@js-soft.com>
2022-01-24 13:59:45 +0100

    feat(docker): update dockerfiles and structure

---
+++ test/ui5-app/webapp/test/test.js
@@ -0,0 +1,7 @@
+describe('basics tests', () => {
+    it('should check the page title', async () => {
+        await browser.url('http://app:8888/index.html');
+        const title = await browser.getTitle();
+        expect(title).toEqual('AMS-UI v-Mock');
+    });
+});
--- test/wdio-browser-docker.conf.js
+++ test/wdio-browser-docker.conf.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 exports.config = {
     // TODO: change to real wdi5 tests
-    specs: [path.join('test', 'e2e', '*.test.js')],
+    specs: [path.join('test', 'test.js')],
     hostname: 'selenium-hub', // tests running inside the container should connect to the same network
     port: 4444,
     path: '/wd/hub',
@@ -11,25 +11,33 @@ exports.config = {
         {
             maxInstances: 1,
             browserName: 'chrome',
+            acceptInsecureCerts: true,
             'goog:chromeOptions': {
                 w3c: false,
-                args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--whitelisted-ips="selenium-hub"']
-            },
-            acceptInsecureCerts: true
+                args: [
+                    '--headless',
+                    '--no-sandbox',
+                    '--disable-dev-shm-usage',
+                    '--whitelisted-ips="selenium-hub"',
+                    '--verbose',
+                    '--ignore-certificate-errors',
+                    '--allow-insecure-localhost'
+                ]
+            }
         }
     ],
     wdi5: {
         // path: "", // commented out to use the default paths
         screenshotPath: path.join('test', 'report', 'screenshots'),
-        logLevel: 'verbose', // error | verbose | silent
+        logLevel: 'error', // error | verbose | silent
         platform: 'browser', // electron, browser, android, ios
         url: '',
         deviceType: 'web'
     },
     services: ['ui5'],
-    logLevel: 'trace',
+    logLevel: 'error',
     logLevels: {
-        webdriver: 'info'
+        webdriver: 'error'
     },
     baseUrl: 'http://app:8888/',
     bail: 0,
